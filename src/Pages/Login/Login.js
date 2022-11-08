078d3eff9ee6bee61824c8cea27b6deb30a20879
mdsadikul <mdsadikulislam891@gamil.com>
2022-11-08 13:36:47 +0600

    Added user login successful

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexs/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const {login} = useContext(AuthContext);
+
     const handleSignIn = event => {
         event.preventDefault();
 
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        
+        login(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err))
     }
     return (
         <div className=''>
